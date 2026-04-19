1653675eb3a6f9bdb6c54b37beadab8442f61e19
Hafsa Zayem <zayem.hafsa@gmail.com>
2026-04-19 17:49:07 +0100

    correct text

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello World evryonet ');
+  res.send('Hello World');
 });
 
 app.get('/sum', (req, res) => {
